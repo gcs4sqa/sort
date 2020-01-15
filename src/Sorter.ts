5ae14230d0afffd2212360a6b5522b8af9483014
cliff <cliff.douglas@digital.homeoffice.gov.uk>
2020-01-15 20:25:09 +0000

    looking at unions and type guards

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -1,22 +1,21 @@
-class Sorter {
+export class Sorter {
     
 
-    constructor(public collection:number[] | string){}
+    constructor(public collection: /*todo fix me*/){}
 
     sort(): void{
 
         const { length } = this.collection;
-    // all of this only works if collection is number[]
-        // if collection is an array of numbers 
+   
         for(let i = 0; i < length; i++){
             for(let j = 0; j < length -i -1; j++){
-             if (this.collection instanceof Array){ //this is a type guard for a non primative type
+            
                 if (this.collection[j] > this.collection[j +1]){
                  const leftHand = this.collection[j];
                  this.collection[j] = this.collection[j+1];
                  this.collection[j+1] = leftHand;
              }   
-            }
+            
             //if collection is a string, do this logic instead
             if(typeof this.collection === 'string'){ //this is a type guard from primative types
 
@@ -27,11 +26,5 @@ class Sorter {
         
 
     }
-        
 }
-
-
-const sorter = new Sorter([10 , 3, -5, 0]);
-
-sorter.sort();
-console.log(sorter.collection);
+        
